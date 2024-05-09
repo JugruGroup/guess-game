c83fe7a52e5a55904ef5410f4d0cd4e1f2cd4b54
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2024-05-09 17:52:13 +0300

    Add Heisenbug 2024 Spring information

--- guess-game-server/src/main/java/guess/util/load/ConferenceDataLoaderExecutor.java
+++ guess-game-server/src/main/java/guess/util/load/ConferenceDataLoaderExecutor.java
@@ -2655,12 +2655,19 @@ public class ConferenceDataLoaderExecutor {
 //                LoadSettings.eventTemplateAndInvalidTalksSet(
 //                        createEventTemplate("TechTrain 2024 Spring", null, List.of(24L)),
 //                        Set.of("Открытие фестиваля TechTrain 2024 Spring", "Закрытие фестиваля TechTrain 2024 Spring")));
-//        loadTalksSpeakersEvent(Conference.HOLY_JS, LocalDate.of(2023, 4, 15), "2024 Spring",
+//        loadTalksSpeakersEvent(Conference.HOLY_JS, LocalDate.of(2024, 4, 15), "2024 Spring",
 //                LoadSettings.eventTemplateAndInvalidTalksSet(
 //                        createEventTemplate("HolyJS 2024 Spring", null, List.of(24L, 7L)),
 //                        Set.of("Открытие конференции HolyJS 2024 Spring",
 //                                "Подведение итогов онлайн-части конференции HolyJS 2024 Spring",
 //                                "Открытие офлайн-части HolyJS 2024 Spring", "Закрытие конференции HolyJS 2024 Spring",
 //                                "Просмотр документального фильма", "Code in the Dark", "Открытый микрофон")));
+//        loadTalksSpeakersEvent(Conference.HEISENBUG, LocalDate.of(2024, 4, 16), "2024 Spring",
+//                LoadSettings.eventTemplateAndInvalidTalksSet(
+//                        createEventTemplate("Heisenbug 2024 Spring", null, List.of(24L, 7L)),
+//                        Set.of("Открытие онлайн-части конференции Heisenbug 2024 Spring",
+//                                "Подведение итогов онлайн-части конференции Heisenbug 2024 Spring",
+//                                "Открытие офлайн-части конференции Heisenbug 2024 Spring", "Lightning Talks",
+//                                "Закрытие конференции Heisenbug 2024 Spring", "Как следить за трендами в IT-индустрии?")));
     }
 }
